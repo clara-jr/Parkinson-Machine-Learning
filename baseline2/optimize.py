e075105c68c81ec7e741ab0fa23a2cd0278ad79d
Clara Jimenez <clarajimenezrecio@gmail.com>
2016-02-04 14:46:25 +0100

    Baseline2 (Using Parallel)

--- baseline2/optimize.py
+++ baseline2/optimize.py
@@ -18,21 +18,31 @@ def frange(start, stop, step=1.0):
         start +=step
         step *= 10
 
-# Greedy - Find optimal parameter C for L=1.0 static -> Find optimal parameter L for C=opt[0] static
+# Greedy - Find optimal parameter C for L=1.0 static -> Find optimal parameter L for C=optimalC static
 
 params = [1.0E-3, 1.0] # [C, L]
-init = [1.0E-5, 1.0E-5]
+init = [1.0E-5, 0.1]
 end = [2, 2]
-step = [9.0E-5, 9.0E-5]
+step = [9.0E-5, 0.9]
+experiments = [6, 2] # C = 0.00001 0.0001 0.001 0.01 0.1 1.0; L = 0.1 1.0
 
-for i in range(len(params)):
+for cont in range(len(params)):
     values = []
     spearman = []
-    for param in frange(init[i], end[i], step[i]): # 0.00001 0.0001 0.001 0.01 0.1 1
-        params[i] = param
+    string = []
+    parallel = "parallel -j " + str(experiments[cont]) + " ./baseline_train_devel.sh {1} {2}"
+    for i in range(len(params)):
+        string.append(" ::: " + str(params[i]))
+    string[cont] = " :::"
+    for param in frange(init[cont], end[cont], step[cont]):
         values.append(param)
-        print "Training C="+str(params[0])+" L="+str(params[1])
-        os.system('./baseline_train_devel.sh '+str(params[0])+' '+str(params[1]))
+        string[cont] += " " + str(param)
+    for i in range(len(params)):
+        parallel += string[i]
+    print "Training " + parallel
+    os.system(parallel)
+    for exp in range(experiments[cont]):
+        params[cont] = values[exp]
         file = open('eval/train_devel/ComParE2015_Parkinson.SVR.C'+str(params[0])+'.L'+str(params[1])+'.result', 'r')
         data = file.readlines();
         for x in data:
@@ -41,7 +51,7 @@ for i in range(len(params)):
         spearman.append(float(s))
         file.close()
     index = spearman.index(max(spearman))
-    params[i] = values[index]
+    params[cont] = values[index]
 
 print "Optimal C = " + str(params[0])
 print "Optimal L = " + str(params[1])
